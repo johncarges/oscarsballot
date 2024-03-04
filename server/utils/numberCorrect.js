b7bb3bee8ef077ea02e02212471165ec03d4e269
John Carges <johncarges@gmail.com>
2024-03-04 17:05:34 -0500

    Another fix to number correct

--- server/utils/numberCorrect.js
+++ server/utils/numberCorrect.js
@@ -6,7 +6,7 @@ module.exports = function numberCorrect(user, results) {
     // results[0]: {_id: (awardId), winner: (winning Nominee Id)}
     // user.responses[0]: {award: (awardId), response: (winning nominee Id)}
     // Need result._id === response.award AND result.winner === response.response
-
+    if (user.responses.length === 0) return 0
     const correct = results.filter(result => {
         const response = user.responses.filter(response=>response.award===result._id)[0]
         return response.response === result.winner
